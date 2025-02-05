55e6f9db98c0b221b8b4dc7331d0f5fdd31611b0
Damien Vallet <damienjdvallet@gmail.com>
2025-02-05 15:44:57 +0000

    ui update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -14,29 +14,31 @@ import React from 'react';
 import { FaGithub, FaLinkedin, FaMastodon } from "react-icons/fa";
 
 
-function Footer() {
+const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="container container-fluid">
-                <div className="row p-2">
-                    <div className="col d-flex justify-content-center align-items-end bottom">
+        <footer className="footer ">
+            <div className="container container-fluid ">
+                <div className="row justify-content-center p-2 ">
+                    <div className="col d-flex justify-content-start align-items-end bottom">
                         <p className="copyright">© 2025 Damien Vallet</p>
                     </div>
-                    <div className="col d-flex justify-content-center align-items-center">
-                        <a href="https://www.linkedin.com/in/damien-vallet-84464195/">
-                            <FaLinkedin className="social-icons" />
-                        </a>
-                    </div>
-                    <div className="col d-flex justify-content-center align-items-center" >
-                        <a href="https://github.com/damienvallet">
-                            <FaGithub className="social-icons" />
-                        </a>
-                    </div>
-                    <div className="col d-flex justify-content-center align-items-center">
-                        <a href="https://mastodon.social/@darladada">
-                            <FaMastodon className="social-icons" />
-                        </a>
-                    </div>
+                    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                        <li className="col d-flex justify-content-end">
+                            <a href="https://www.linkedin.com/in/damien-vallet-84464195/">
+                                <FaLinkedin className="social-icons" />
+                            </a>
+                        </li>
+                        <li className="col d-flex justify-content-end" >
+                            <a href="https://github.com/damienvallet">
+                                <FaGithub className="social-icons" />
+                            </a>
+                        </li>
+                        <li className="col d-flex justify-content-end">
+                            <a href="https://mastodon.social/@darladada">
+                                <FaMastodon className="social-icons" />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </footer>
