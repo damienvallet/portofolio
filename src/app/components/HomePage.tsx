1c0addb588942e69c8e346fdc510751b35604850
Damien Vallet <damienjdvallet@gmail.com>
2025-02-17 15:31:07 +0000

    update ui

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -42,30 +42,19 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="d-flex flex-column container-fluid home">
-      <div className="row justify-content-center align-items-center">
-        <div className="col">
-          <h1 className="title text-center font-weight-bold">
-            Damien Vallet
-          </h1>
-          <div className="d-flex row justify-content-center align-items-center">
-            <div className="col">
-              <h2 className={`tagline ${fade ? "fade-in" : "fade-out"}`}>
-                <b suppressHydrationWarning>{tagline}</b>
-              </h2>
-            </div>
-          </div>
-          <div className="d-flex p-3 justify-content-center align-items-center">
-              <p className="biography mx-auto text-center">
-                <b>About me : </b>
-                <br />
-                I&apos;m a sound activist with over a decade of experience in
-                the audio software industry.
-              </p>
-          </div>
-        </div>
+    <main className="container-fluid home d-flex flex-column justify-content-center align-items-center text-center">
+      <div className="col-12 col-md-8">
+        <h1 className="title font-weight-bold">Damien Vallet</h1>
+        <h2 className={`tagline ${fade ? "fade-in" : "fade-out"}`}>
+          <b suppressHydrationWarning>{tagline}</b>
+        </h2>
+        <p className="biography mx-auto">
+          <b>About me : </b>
+          <br />
+          I&apos;m a sound activist with over a decade of experience in the audio software industry.
+        </p>
       </div>
-    </div>
+    </main>
   );
 };
 export default HomePage;
