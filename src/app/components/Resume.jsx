ae41767fa1a9bfe6e68d4cad23198bc737fb229c
Damien Vallet <damienjdvallet@gmail.com>
2025-04-16 21:00:20 +0000

    edit resume

--- src/app/components/Resume.jsx
+++ src/app/components/Resume.jsx
@@ -3,8 +3,7 @@
  * Description: Resume component
  * Author: Damien Vallet
  * Version: 1.0
- * Date: 2024
- * License: 
+ * Date: 2025
  * Language: JavaScript
  */
 
@@ -26,36 +25,36 @@ function Resume() {
                         I&apos;m a sound activist with over a decade of experience in the audio software industry. Specializing in sound design and music production, I&apos;ve dedicated my career to creating innovative soundware and audio products. My passion for music and sound continues to drive me, and I&apos;m always exploring new ways to craft unique audio experiences that push creative boundaries.
                     </p>
                     <p>
-                        Naturally, I've added to my skills some programming knowledge to sprinkle a bit of magic into my sound design. After being stroke by a genetic cancer and survived from the worst experience of my existence, I had decided to go back to school to learn programming.
-                        I've always had a strong interest in programming. I am currently learning C++ and JUCE to help my audio creativity. I am also interested in web development, and I am currently learning different technologies such as React, Next.js, and Node.js to create web applications (No template has been hurt in the process).
+                        Naturally, I&apos;ve added to my skills some programming knowledge to sprinkle a bit of magic into my sound design. After surviving an agressive genetic cancer, I made the decision to go back to school and learn programming.
+                        {/* In retrospect, I&apos;ve always had a strong interest in programming. */} I&apos;m currently working around C++ and JUCE. I&apos;m also interested in web development, and I&apos;m currently learning different technologies such as React, Next.js, and Node.js to create web applications (No template has been hurt in the process).
                     </p>
                     <p>
-                        I've always been engaged in human rights and social justice issues, and I am particularly involved in the cancer community and the right to die with dignity. I'm also the father of a child with dwarfism so I am engaged in the fight against discrimination and for the rights of people with disabilities.
-                        I'm an elected member of the city council of my hometown, where I work to improve the quality of life for all citizens. I'm particularly engaged in issues related to food security, the environment, and citizen participation. I truly believe that local politics is the best place to make a difference.
+                        I&apos;ve always been engaged in human rights and social justice issues, and I&apos;m particularly involved in the cancer community and the right to die with dignity. I&apos;m also the father of a child with dwarfism so I&apos;m engaged in the fight against discrimination and for the rights of people with disabilities.
+                        I&apos;m an elected member of the city council of my hometown, where I work to improve the quality of life for all citizens. I&apos;m particularly engaged in issues related to food security, the environment, and citizen participation. I truly believe that local politics is the best place to make a difference.
                     </p>
                     <p>
-                        I'm committed to make a positive impact in the world, and I believe that social change comes from the bottom up. I am convinced that we can all make a difference, and I am determined to do my part.
+                        I&apos;m committed to make a positive impact in the world, and I believe that social change comes from the bottom up. I&apos;m convinced that we can all make a difference, and I&apos;m determined to do my part.
                     </p>
                 </div>
             </div>
             <div className="row p-5">
-                <div className="col p-3">
+                <div className="col p-3 text-center">
                     <a
                         href="/DV-CV2025_en.pdf"
                         target="_blank"
                         rel="noreferrer"
-                        className="btn btn-primary"
+                        className="btn btn-primary btn-download"
                         download
                     >
                         Download Resume English version
                     </a>
                 </div>
-                <div className="col p-3">
+                <div className="col p-3 text-center">
                     <a
                         href="/DV-CV2025_fr.pdf"
                         target="_blank"
                         rel="noreferrer"
-                        className="btn btn-primary"
+                        className="btn btn-primary btn-download"
                         download
                     >
                         Télécharger CV version française
